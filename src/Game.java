f151905bfb27932c79d0870469c4eb71aab910b7
kaneko-shoi <kaneko-shoi-ps@ynu.jp>
2023-06-27 14:34:24 +0900

    Game.java,今度こそ二次元配列の削除

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,6 @@
 public class Game {
     int turn;   //手番
     int[][] board = new int[8][12];
-    int[][] remainAction = new int[2][2];
-    boolean[][] isDead = new boolean[2][2];
+    int[] remainAction = new int[2];
+    boolean[] isDead = new boolean[2];
 }
