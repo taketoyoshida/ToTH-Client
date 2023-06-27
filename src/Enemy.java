a1a6c40fc1081d7f9212e37c4f50c861e1371cc7
(Kodai Nagura) <koudai.nagura@gmail.com>
2023-06-27 15:26:08 +0900

    敵の作り方 どう画像と結びつけるかはまだやってない

--- src/Enemy.java
+++ src/Enemy.java
@@ -2,6 +2,7 @@ public class Enemy {
 
     // 敵の例
     EnemyStatus mob1 = new EnemyStatus(1,"Mob1", 1,1,1,1,1);
+    EnemyStatus mob2 = new EnemyStatus(2,"Mob2", 2,2,1,1,1);
 
     void action(){
         //動き方
@@ -24,6 +25,7 @@ class EnemyStatus{
     int LV;
 
     public EnemyStatus(int id,String name, int hp, int atk, int mov,int rng, int lv){
+        // HPと攻撃
         this.ID = id;
         this.name = name;
         this.HP = hp;
