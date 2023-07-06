c6d612141681e207e98193b703d8f3f12a24e4fc
Nagura <koudai.nagura@gmail.com>
2023-07-06 22:37:43 +0900

    Users クラスの追加

---
+++ src/main/java/model/Users.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Map;
+
+public class Users {
+    int ID;
+    String acountName;
+    //所持装備・素材・設計図
+    private Map<Material, Integer> materials;   // 素材
+    int Balance;
+    int rank;
+
+
+
+}
