355c147a5efc524aff6724bb5b08faeed7b1ecf0
Shion Ichikawa <shion1305@gmail.com>
2023-07-07 03:58:23 +0900

    ✨ User.java add initializer, getMaterials, addMaterial, consumeMaterial

--- src/main/java/model/util/User.java
+++ src/main/java/model/util/User.java
@@ -2,15 +2,25 @@ package model.util;
 
 import model.Material;
 
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
 
 public class User {
     int ID;
     private String username;
     //所持装備・素材・設計図
-    private Map<Material, Integer> materials;   // 素材
+    private EnumMap<Material, Integer> materials;   // 素材
     private int balance;
     private int rank;
+
+    public User(int id, String username, int balance, int rank) {
+        this.ID = id;
+        this.username = username;
+        this.balance = balance;
+        this.rank = rank;
+        this.materials = new EnumMap<>(Material.class);
+    }
 
     public void setUsername(String username) {
         this.username = username;
@@ -41,5 +51,20 @@ public class User {
 
     public int getRank() {
         return rank;
+    }
+
+    public Map<Material, Integer> getMaterials() {
+        return materials;
+    }
+
+    public void addMaterial(Material material, int amount) {
+        materials.put(material, materials.getOrDefault(material, 0) + amount);
+    }
+
+    public void consumeMaterial(Material material, int amount) throws Exception {
+        if (materials.getOrDefault(material, 0) < amount) {
+            throw new Exception("Not enough materials");
+        }
+        materials.put(material, materials.get(material) - amount);
     }
 }
