58513269137d8f5a9de9828aa7fc0a3240db7cf6
Nagura <koudai.nagura@gmail.com>
2023-07-06 22:53:48 +0900

    修正

--- src/main/java/model/util/User.java
+++ src/main/java/model/util/User.java
@@ -8,7 +8,7 @@ public class User {
     private String userName;
     //所持装備・素材・設計図
     private Map<Material, Integer> materials;   // 素材
-    private int Balance;
+    private int balance;
     private int rank;
 
 
