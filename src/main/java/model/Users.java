4bacc7d98eda64f9a73977a90b9897d858f5c637
Nagura <koudai.nagura@gmail.com>
2023-07-06 22:45:47 +0900

    acountName -> userName

--- src/main/java/model/Users.java
+++ src/main/java/model/Users.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class Users {
     int ID;
-    String acountName;
+    String userName;
     //所持装備・素材・設計図
     private Map<Material, Integer> materials;   // 素材
     int Balance;
