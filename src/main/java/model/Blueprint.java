e0368f56ad1698571be1eef80e10fe9fe3b2db50
Shion Ichikawa <shion1305@gmail.com>
2023-07-06 23:09:25 +0900

    🎨 change Blueprint to contain EquipmentItem as baseItem

--- src/main/java/model/Blueprint.java
+++ src/main/java/model/Blueprint.java
@@ -1,24 +1,9 @@
 package model;
 
 public class Blueprint {
-    int ID;
-    String name;
+    public final EquipmentItem baseItem;
 
-    double rate;
-
-    public Blueprint(int id,String name,double rate){
-        this.ID=id;
-        this.name=name;
-        this.rate=rate;
+    public Blueprint(EquipmentItem baseItem) {
+        this.baseItem = baseItem;
     }
-
-    public double getRate(){
-        return rate;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-
 }
