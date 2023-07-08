93ed7e825493cb7e1658823280de3c1df0d53f59
Nagura <koudai.nagura@gmail.com>
2023-07-08 22:40:13 +0900

    装備の情報・素材の情報・ガチャの排出率（？）を記述＆「視野」を削除

--- src/main/java/model/Status.java
+++ src/main/java/model/Status.java
@@ -4,14 +4,12 @@ public class Status {
     private int HP;     // HP
     private int ATK;    // 攻撃力
     private int MOV;    // 行動力
-    private int EYE;    // 視野
     private int RNG;    // 射程
 
-    public Status(int HP, int ATK, int MOV, int EYE, int RNG) {
+    public Status(int HP, int ATK, int MOV,  int RNG) {
         this.HP = HP;
         this.ATK = ATK;
         this.MOV = MOV;
-        this.EYE = EYE;
         this.RNG = RNG;
     }
 
@@ -25,10 +23,6 @@ public class Status {
 
     public int getMOV() {
         return MOV;
-    }
-
-    public int getEYE() {
-        return EYE;
     }
 
     public int getRNG() {
