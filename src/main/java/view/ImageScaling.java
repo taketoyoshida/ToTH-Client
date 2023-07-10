7f05d74e8aef60d5abe9e6fb30da8355956a5f29
kaneko-shoi <kaneko-shoi-ps@ynu.jp>
2023-07-10 23:21:23 +0900

    整形

--- src/main/java/view/ImageScaling.java
+++ src/main/java/view/ImageScaling.java
@@ -9,17 +9,18 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class ImageScaling extends JFrame{       //画像の拡大縮小を行うクラス
+public class ImageScaling extends JFrame {       //画像の拡大縮小を行うクラス
     ImageIcon icon;
     MediaTracker mt;                            //画像処理の終了を待つために必要らしい
-    public ImageScaling(){
+
+    public ImageScaling() {
         icon = new ImageIcon();
         this.mt = new MediaTracker(this);
     }
 
     /*拡大縮小したいImageIconと拡大率を引数とするメソッド*/
     /*拡大縮小されたImageIconを返す*/
-    public ImageIcon scale(ImageIcon input,double s){
+    public ImageIcon scale(ImageIcon input, double s) {
         icon = input;
         /*getScaledInstanceにおいて、アス比を変えないなら高さは-1でいいらしい*/
         Image scaled = icon.getImage().getScaledInstance((int) (icon.getIconWidth() * s), -1,
