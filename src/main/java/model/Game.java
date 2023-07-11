4568fc8941bd8ee5edcb7f0584a607ca9d78bfaa
Shion Ichikawa <shion1305@gmail.com>
2023-07-11 14:07:21 +0900

    💥 change implementation of model.Game, introduce Piece.java

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,8 +1,32 @@
 package model;
 
+import java.util.HashMap;
+
 public class Game {
-    int turn;   //手番
-    int[][] board = new int[8][12];
+    public static final int BOARD_ROW = 8;
+    public static final int BOARD_COL = 12;
+    public int turn;   //手番
+    private final HashMap<Position, Piece> board;
     int[] remainAction = new int[2];
-    boolean[] isDead = new boolean[2];
+    public final boolean[] isDead;
+
+    public Game() {
+        turn = 0;
+        remainAction[0] = 2;
+        remainAction[1] = 2;
+        isDead = new boolean[]{false, false};
+        board = new HashMap<>();
+    }
+
+    public void setPiece(Position pos, Piece piece) {
+        board.put(pos, piece);
+    }
+
+    public Piece getPiece(Position pos) {
+        return board.get(pos);
+    }
+
+    public Piece getPiece(int x, int y) {
+        return board.get(new Position(x, y));
+    }
 }
---
+++ src/main/java/model/Piece.java
@@ -0,0 +1,12 @@
+package model;
+
+
+import java.io.Serial;
+import java.io.Serializable;
+
+// Board上のコマを表すクラス。Player, Enemy, Obstacleの親クラス
+public class Piece implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 12034L;
+
+}
