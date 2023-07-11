880046a0ace483f61d2a0b5e9ebc6fb3f2e3a26e
Shion Ichikawa <shion1305@gmail.com>
2023-07-11 14:40:43 +0900

    🚧 Game with Gateway, model.Game

--- src/main/java/controller/game/Game.java
+++ src/main/java/controller/game/Game.java
@@ -1,23 +1,37 @@
 package controller.game;
 
-import model.EnemyStatus;
+import controller.networking.GameGateway;
+import model.Piece;
 import model.Player;
-import model.Status;
 import model.Position;
+import model.Status;
 
 import java.util.Random;
+
+import static model.Game.BOARD_COL;
+import static model.Game.BOARD_ROW;
 
 public class Game {
     private Random random;
     private static final int GAME_TURN = 15;
-    private static final int BOARD_ROW = 8;
-    private static final int BOARD_COL = 12;
     private int turnNum;
-    private int[][] board;
+    private final model.Game game;
     private int[] enemy;
-    Player Player1 = new Player("Player1", new Status(1, 1, 1, 1));
-    Player Player2 = new Player("Player2", new Status(1, 1, 1, 1));
+    private final GameGateway.IGameGateway gateway;
+    private final Player me = new Player("Player1", new Status(1, 1, 1, 1));
+    private final Player opponent = new Player("Player2", new Status(1, 1, 1, 1));
 
+
+    //ゲームの初期化
+    public Game() {
+        game = new model.Game();
+        setObstacle();
+        setEnemy();
+        turnNum = 0;
+        random = new Random();
+        // TODO: Gateway should be properly initialized
+        this.gateway = new GameGateway.IGameGateway() {};
+    }
 
     //ゲームのメインループ
     public void playGame() {
@@ -37,35 +51,25 @@ public class Game {
             refBoard();
             refStatus();
 
-            if (Player1.isDead()) {
+            if (me.isDead()) {
 
             }
-            if (Player2.isDead()) {
+            if (opponent.isDead()) {
 
             }
         }
 
-        if (Player1.getScore() == Player2.getScore()) {
+        if (me.getScore() == opponent.getScore()) {
             /* 引き分け */
-        } else if (Player1.getScore() > Player2.getScore()) {
+        } else if (me.getScore() > opponent.getScore()) {
             /* プレイヤ－1の勝ち */
-        } else if (Player1.getScore() < Player2.getScore()) {
+        } else if (me.getScore() < opponent.getScore()) {
             /* プレイヤー2の勝ち */
         }
 
         /* 獲得ゴールドの計算 */
     }
 
-
-    //ゲームの初期化
-    public void initialize() {
-        board = new int[BOARD_ROW][BOARD_COL];
-        setObstacle();
-        board[7][0] = 1;  //プレイヤー初期位置
-        board[0][11] = 2; //プレイヤー初期位置
-        setEnemy();
-        turnNum = 0;
-    }
 
     //盤面を更新
     public void refBoard() {
@@ -118,42 +122,45 @@ public class Game {
     //敵をセット
     public void setEnemy() {
         int rank;
-        if (Player1.getRank() < Player2.getRank()) {
-            rank = Player1.getRank();
+        if (me.getRank() < opponent.getRank()) {
+            rank = me.getRank();
         } else {
-            rank = Player2.getRank();
+            rank = opponent.getRank();
         }
         enemy = new int[4];
         int i;
-        for(i=1;i<=4;i++) {
+        for (i = 1; i <= 4; i++) {
             enemy[i] = rank * 2 - i;    // とりあえずIDを入れる
             if (rank * 2 - i <= 0) break;// rank1のみ敵は２種類
         }
-        board[0][0] = enemy[i-2];   // rank1以外は１つ前のrankで初登場した敵が対角に初期配置
-        board[7][11] = enemy[i-1];  // rank1ではrank1の敵２種が配置
+        // TODO: Implement this using Piece class
+//        game.board[0][0] = enemy[i - 2];   // rank1以外は１つ前のrankで初登場した敵が対角に初期配置
+//        game.board[7][11] = enemy[i - 1];  // rank1ではrank1の敵２種が配置
     }
 
     // 敵を特定のマスに置く
     public void putEnemy() {
-        int ROW, COL;
         do {
-            ROW = RandomNum(0, BOARD_ROW - 1);// ランダムで座標を生成
-            COL = RandomNum(0, BOARD_COL - 1);
-            if(board[ROW][COL]!=0) continue;// 当該マスに障害物含む何かがあったら再生成
-            if (ObjectCheck(ROW, COL, 3)) {
-                board[ROW][COL] = enemy[RandomNum(0, 3)];// 周囲1マスに他のプレイヤーやモンスターが無ければランダムで敵をセット
-                break;
-            }
-        }while(true);// 敵がセットされるまで続く
+            // 周囲1マスに他のプレイヤーやモンスターが無ければランダムで敵をセット
+            int ROW = getRandomNum(0, BOARD_ROW - 1);// ランダムで座標を生成
+            int COL = getRandomNum(0, BOARD_COL - 1);
+            if (game.getPiece(ROW, COL) != null
+                    || game.getPiecesAround(new Position(ROW, COL)).size() == 0) continue;// 当該マスに障害物含む何かがあったら再生成
+            // TODO: Implement Piece
+            game.setPiece(new Position(ROW,COL),new Piece());
+            break;
+        } while (true);// 敵がセットされるまで続く
     }
 
     //障害物を最大10個セット
     public void setObstacle() {
-        int ROW,COL;
-        for(int i=0;i<10;i++) {
-            ROW = RandomNum(0, BOARD_ROW - 1);// ランダムで座標を生成
-            COL = RandomNum(0, BOARD_COL - 1);
-            if(ObjectCheck(ROW,COL,0)) board[ROW][COL] = 3;// 周囲1マスに他の障害物が無ければセット
+        int ROW, COL;
+        for (int i = 0; i < 10; i++) {
+            ROW = random.nextInt(0, BOARD_ROW);// ランダムで座標を生成
+            COL = random.nextInt(0, BOARD_COL);
+            // TODO: Implement Piece
+            if (game.getPiecesAround(new Position(ROW, COL)).size() == 0)
+                game.setPiece(new Position(ROW, COL), new Piece());// 周囲1マスに他の障害物が無ければセット
         }
     }
 
@@ -165,24 +172,13 @@ public class Game {
         }
     }
 
-    public int RandomNum(int min, int max) {
+    public int getRandomNum(int min, int max) {
         return random.nextInt(max - min + 1) + min;
     }
 
-    // 周囲にオブジェクトがあるかチェック
-    public boolean ObjectCheck(int ROW,int COL,int without) {// withoutに指定したオブジェクトは無視
-        int X=0;
-        for(int j=-1;j<=1;j++){
-            for(int k=-1;k<=1;k++){
-                if(board[ROW+j][COL+k]!=0 && board[ROW+j][COL+k]!=without) return false;
-            }
-        }
-        return true;
-    }
 
     public static void main(String[] args) {
         Game game = new Game();
-        game.initialize();
         game.printBoard();
         game.playGame();
     }
