c9fcdcf2da355f2d55dc1736c4f7e40ac4faa09f
Shion Ichikawa <shion1305@gmail.com>
2023-07-12 21:43:21 +0900

    🎨 変数名の変更 / コメント追加

--- src/main/java/view/Workshop.java
+++ src/main/java/view/Workshop.java
@@ -4,42 +4,44 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import static java.awt.Font.BOLD;
 
 public class Workshop extends JFrame implements ActionListener {
 
     private final WindowBase base;
-    private JLayeredPane p = new JLayeredPane();
-    private JLayeredPane p2 = new JLayeredPane();
+    private JLayeredPane menuPanel = new JLayeredPane();
+    private JLayeredPane equipInfoPane = new JLayeredPane();
     private JLayeredPane listPane = new JLayeredPane();
-    private ImageIcon icon1 = new ImageIcon("./assets/imgs/ログイン画面.png");    //画像のディレクトリは調整してもろて
-    private ImageIcon listIcon = new ImageIcon("./assets/imgs/TestEquipBaseList.png");
-    private ImageIcon icon2 = new ImageIcon("./assets/imgs/TestWorkshopText.png");
-    private ImageIcon icon3 = new ImageIcon("./assets/imgs/TestEquipSlot.png");
-    private ImageIcon bIcon = new ImageIcon("./assets/imgs/TestButton3.png");
+    private ImageIcon iconBackground = new ImageIcon("./assets/imgs/ログイン画面.png");    //画像のディレクトリは調整してもろて
+    private ImageIcon iconList = new ImageIcon("./assets/imgs/TestEquipBaseList.png");
+    private ImageIcon iconEquipPickArea = new ImageIcon("./assets/imgs/TestWorkshopText.png");
+    private ImageIcon iconUpgradeSelection = new ImageIcon("./assets/imgs/TestEquipSlot.png");
+    private ImageIcon iconUpgrade = new ImageIcon("./assets/imgs/TestButton3.png");
+    private ImageIcon iconItem = new ImageIcon("./assets/imgs/TestItemShield.png");
+
     //ImageIcon icon2 = new ImageIcon("./assets/imgs/エルフ.jpg");
     ImageScaling isc = new ImageScaling();
 
-    JLabel label1 = new JLabel(icon1);        //画像はlabelで取り込む
-    JLabel label2 = new JLabel(icon2);
-    JLabel label3 = new JLabel(icon3);
-    JLabel label4 = new JLabel(icon3);
+    JLabel label1 = new JLabel(iconBackground);        //画像はlabelで取り込む
+    JLabel label2 = new JLabel(iconEquipPickArea);
+    JLabel labelUpgradeBefore = new JLabel(iconUpgradeSelection);
+    JLabel labelUpgradeAfter = new JLabel(iconUpgradeSelection);
     JLabel[] textLabel = new JLabel[6];
 
-    JScrollPane sp = new JScrollPane();
-    JViewport vp = sp.getViewport();
-    JButton b1 = new JButton(bIcon);
+    JScrollPane scrollPane = new JScrollPane();
+    // viewportにscrollPaneの中のコンテンツを格納
+    // scrollPane>viewport>listPane>itemButton
+    JViewport viewport = scrollPane.getViewport();
+    JButton buttonUpgrade = new JButton(iconUpgrade);
 
 
     public Workshop(WindowBase base) {
 
         this.base = base;
         label1.setBounds(0, 0, 816, 512);//背景の描画とレイヤーの設定
-        p.add(label1);
-        p.setLayer(label1, -10);
+        menuPanel.add(label1);
+        menuPanel.setLayer(label1, -10);
 
         for (int i = 0; i < textLabel.length; i++) {
             textLabel[i] = new JLabel();
@@ -47,62 +49,60 @@ public class Workshop extends JFrame implements ActionListener {
 
         start();
 
-        base.change(p);
-
+        this.base.change(menuPanel);
 
     }
 
     public void menu() {//ボタンのみなさんの召喚
 
-        p.setLayout(null);      //ボタン配置の設定
+        menuPanel.setLayout(null);      //ボタン配置の設定
 
         getList();
-        vp.setView(listPane);
-        sp.setBounds(16, 16, 176 + 18, 480);
-        p.add(sp);
-        p.setLayer(sp, 10);
+        viewport.setView(listPane);
+        scrollPane.setBounds(16, 16, 176 + 18, 480);
+        menuPanel.add(scrollPane);
+        menuPanel.setLayer(scrollPane, 10);
 
         label2.setBounds(226, 336, 574, 160);
-        p.add(label2);
-        p.setLayer(label2, 0);
+        menuPanel.add(label2);
+        menuPanel.setLayer(label2, 0);
 
-        label3.setBounds(242, 72, 192, 192);
-        p.add(label3);
-        p.setLayer(label3, 0);
-        label4.setBounds(592, 72, 192, 192);
-        p.add(label4);
-        p.setLayer(label4, 0);
+        labelUpgradeBefore.setBounds(242, 72, 192, 192);
+        menuPanel.add(labelUpgradeBefore);
+        menuPanel.setLayer(labelUpgradeBefore, 0);
+        labelUpgradeAfter.setBounds(592, 72, 192, 192);
+        menuPanel.add(labelUpgradeAfter);
+        menuPanel.setLayer(labelUpgradeAfter, 0);
 
-        b1.setBorderPainted(false);
-        b1.setContentAreaFilled(false);
-        b1.setBounds(450, 136, 126, 64);
-        p.add(b1);
-        b1.addActionListener(this);
-        p.setLayer(b1, 0);
+        buttonUpgrade.setBorderPainted(false);
+        buttonUpgrade.setContentAreaFilled(false);
+        buttonUpgrade.setBounds(450, 136, 126, 64);
+        menuPanel.add(buttonUpgrade);
+        buttonUpgrade.addActionListener(this);
+        menuPanel.setLayer(buttonUpgrade, 0);
     }
 
     public void start() {
         menu();
-        b1.setEnabled(false);
+        buttonUpgrade.setEnabled(false);
         printInfo("装備を選択してください", 0);
-        p2.setBounds(0,0,832,549);
-        p.add(p2);
-        p.setLayer(p2, 10);
+        equipInfoPane.setBounds(0, 0, 832, 549);
+        menuPanel.add(equipInfoPane);
+        menuPanel.setLayer(equipInfoPane, 10);
     }
 
     public void getList() {
         int limit = 15, pf = 480;
         for (int i = 0; i < limit; i++) {
-            ImageIcon iIcon = new ImageIcon("./assets/imgs/TestItemShield.png");
-            JButton iLabel = new JButton(isc.scale(iIcon, 2.0));
-            iLabel.setBounds(16 + 80 * (i % 2), 16 + 80 * (i / 2), 64, 64);
-            listPane.add(iLabel);
-            listPane.setLayer(iLabel, 10);
+            JButton itemButton = new JButton(isc.scale(iconItem, 2.0));
+            itemButton.setBounds(16 + 80 * (i % 2), 16 + 80 * (i / 2), 64, 64);
+            listPane.add(itemButton);
+            listPane.setLayer(itemButton, 10);
         }
         int n = ((int) Math.ceil((double) limit / 2.0)) * 80 + 16;
         if (n > 480) pf = n;
         for (int i = 0; i * 80 - 64 <= pf; i++) {
-            JLabel listLabel = new JLabel(listIcon);
+            JLabel listLabel = new JLabel(iconList);
             listLabel.setBounds(0, i * 80 - 64, 176, 80);
             listPane.add(listLabel);
             listPane.setLayer(listLabel, 0);
@@ -111,18 +111,20 @@ public class Workshop extends JFrame implements ActionListener {
         listPane.setPreferredSize(new Dimension(176, pf));
     }
 
+    //装備の情報を表示する
     private void printInfo(String info, int n) {
         System.out.println("test");
         if (n < 0 || n > textLabel.length) return;
         textLabel[n].setText(info);
         textLabel[n].setFont(new Font("ＭＳ ゴシック", BOLD, 22));
         textLabel[n].setBounds(230, 340 + n * 26, 562, 22);
-        p2.add(textLabel[n]);
-        p2.setLayer(textLabel[n], 10);
+        equipInfoPane.add(textLabel[n]);
+        equipInfoPane.setLayer(textLabel[n], 10);
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == b1) {
+        // Upgradeボタンが押されたときの処理
+        if (e.getSource() == buttonUpgrade) {
             System.out.println("力が…欲しいか…？");
         }
 
